17a02cc680b6266fe8a58fa905abe1e54ef0a5a6
Jonathas-Lopes <jonathaslopes20@gmail.com>
2022-02-23 18:18:39 -0300

    Inclusão de "_id" para facilitar teste

--- server/src/db/schema/cardSchema.js
+++ server/src/db/schema/cardSchema.js
@@ -1,37 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-const Types = ['doacao', 'aviso','venda', 'reserva']
+const Types = ["doacao", "aviso", "venda", "reserva"];
 
 const CardSchema = new Schema({
-  type:{
-    type: String,
-    enum: Types,
-    required: true,
-
-  },
-  title: {
-    type: String,
-    required: true,
-   
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-  body: {
-    type: String,
-    required: true,
-  },
-  _userId: {
-    type: String,
-    required: true,
-  },
-  imgPath:{
-    type: String,
-    required: false,
-    }  
+	_id: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	type: {
+		type: String,
+		enum: Types,
+		required: true,
+	},
+	title: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: false,
+	},
+	body: {
+		type: String,
+		required: true,
+	},
+	_userId: {
+		type: String,
+		required: true,
+	},
+	imgPath: {
+		type: String,
+		required: false,
+	},
 });
-
 
 module.exports = CardSchema;
--- server/src/db/schema/userSchema.js
+++ server/src/db/schema/userSchema.js
@@ -1,43 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  cpf: {
-    type: String,
-    required: true,
-  },
-  apt: {
-    type: String,
-    required: true,
-  },
-  torre:{
-    type: String,
-    required: true
-  },
-  admin:{
-    type: Boolean,
-    required: true,
-    default: false
-  },
-  imgPath:{
-    type: String,
-    required: false,
-    }  
+	_id: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	name: {
+		type: String,
+		required: true,
+	},
+	cpf: {
+		type: String,
+		required: true,
+	},
+	apt: {
+		type: String,
+		required: true,
+	},
+	torre: {
+		type: String,
+		required: true,
+	},
+	admin: {
+		type: Boolean,
+		required: true,
+		default: false,
+	},
+	imgPath: {
+		type: String,
+		required: false,
+	},
 });
-
 
 module.exports = UserSchema;
