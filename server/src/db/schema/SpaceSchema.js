b02bce0bcd1da900fcffb21070fe02d5e36e45d8
Jonathas-Lopes <jonathaslopes20@gmail.com>
2022-02-27 20:18:36 -0300

    Melhoria eslint.

--- server/src/db/schema/SpaceSchema.js
+++ server/src/db/schema/SpaceSchema.js
@@ -1,35 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const SpaceSchema = new Schema({
-  
-  name: {
-    type: String,
-    required: true,
-    
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-  rules: {
-    type: String,
-    required: true,
-  },
-  reservedBy: {
-    type: String,
-    required: true,
-  },
-  timeReserved: {
-    type: String,
-    required: true,
-  },
-  imgPath:{
-    type: String,
-    required: false,
-    }  
+	name: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: false,
+	},
+	rules: {
+		type: String,
+		required: true,
+	},
+	reservedBy: {
+		type: String,
+		required: true,
+	},
+	timeReserved: {
+		type: String,
+		required: true,
+	},
+	imgPath: {
+		type: String,
+		required: false,
+	},
 });
-
 
 module.exports = SpaceSchema;
--- server/src/db/schema/eventSchema.js
+++ server/src/db/schema/eventSchema.js
@@ -1,32 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-
 const EventSchema = new Schema({
-  
-  title: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  date: {
-    type: String,
-    required: true,
-  },
-  body: {
-    type: String,
-    required: true,
-  },
-  _userId: {
-    type: String,
-    required: true,
-  },
-  imgPath:{
-    type: String,
-    required: false,
-    }  
+	title: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	date: {
+		type: String,
+		required: true,
+	},
+	body: {
+		type: String,
+		required: true,
+	},
+	_userId: {
+		type: String,
+		required: true,
+	},
+	imgPath: {
+		type: String,
+		required: false,
+	},
 });
-
 
 module.exports = EventSchema;
