01970ff664c2263fa64fc2bec82a2fd2c3c5e6cb
Jonathas-Lopes <jonathaslopes20@gmail.com>
2022-03-20 19:19:29 -0300

    Adequando para atender comentário.

--- server/src/db/connection/connection.js
+++ server/src/db/connection/connection.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectionConfig = {
-  async connect() {
-    await mongoose.connect(`${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_COLLECTION}`);
-  },
-  db: mongoose.connection,
+	async connect() {
+		return mongoose.connect(
+			`${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_COLLECTION}`
+		);
+	},
+	db: mongoose.connection,
 };
 module.exports = connectionConfig;
